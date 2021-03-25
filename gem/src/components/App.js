0fc3f8f4b0ebf98ac5c5b33a64d9f996cb33c70e
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-03-25 09:37:34 +0900

    React : Tags

--- gem/src/components/App.js
+++ gem/src/components/App.js
@@ -1,11 +1,13 @@
 import Header from './header';
 import Navigation from './Navigation';
+import Tags from './Tags';
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Navigation />
+      <Tags />
     </div>
   );
 }
---
+++ gem/src/components/Tags.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Tags = () => {
+  return (
+    <div class='HomeQuickTag'>
+      <div class='HomeQuickTag__container'>
+        <a href='' class='HomeQuickTag__item'>
+          새로생긴 맛집
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          회식
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          작업하기 좋은 카페
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          데이트하기 좋은 카페
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          디저트 맛집
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          새로생긴 맛집
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          회식
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          작업하기 좋은 카페
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          데이트하기 좋은 카페
+        </a>
+        <a href='' class='HomeQuickTag__item'>
+          디저트 맛집
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Tags;
