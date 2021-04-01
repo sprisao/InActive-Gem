b5766b9a59a4a73837a228877e9dc134426d59b1
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-04-01 14:06:42 +0900

    Navigation 새롭게 세팅 완료

--- gem/src/components/Navigation.js
+++ gem/src/components/Navigation.js
@@ -1,137 +1,21 @@
-import React from 'react';
-import { Image, CloudinaryContext } from 'cloudinary-react';
+import React, { useState } from 'react';
+import { category } from '../datafiles/categories';
+import './Navigation.css';
 
 const Navigation = () => {
+  const [navigation, setNavigation] = useState(category);
   return (
-    <section className='Navigation'>
-      <CloudinaryContext cloudName='diimwnnmj'>
-        <div className='Navigation__wrap'>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='hot_whb0c9'
-              format='jpg'
-            ></Image>
-            <p>인기장소</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='new_v8vqmp'
-              format='jpg'
-            ></Image>
-            <p>새로생긴 곳</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='food_nuddsz'
-              format='jpg'
-            ></Image>
-            <p>맛집</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='cafe_dogisg'
-              format='jpg'
-            ></Image>
-            <p>카페</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='pub_ypq1dw'
-              format='jpg'
-            ></Image>
-            <p>호프</p>
-          </a>
-        </div>
-        <div className='Navigation__wrap'>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='fitness_osoaom'
-              format='jpg'
-            ></Image>
-            <p>운동・피트니스</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='golf_rp6iid'
-              format='jpg'
-            ></Image>
-            <p>스크린골프</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='salon_ciebqq'
-              format='jpg'
-            ></Image>
-            <p>미용실</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='academy_vap3fs'
-              format='jpg'
-            ></Image>
-            <p>학원</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='studio_fne8nw'
-              format='jpg'
-            ></Image>
-            <p>스튜디오</p>
-          </a>
-        </div>
-        <div className='Navigation__wrap'>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='pharmacy_ngaewq'
-              format='jpg'
-            ></Image>
-            <p>약국</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='hospital_ttw46g'
-              format='jpg'
-            ></Image>
-            <p>병원</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='dog_v7ba4i'
-              format='jpg'
-            ></Image>
-            <p>애견</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='shopping_aevysq'
-              format='jpg'
-            ></Image>
-            <p>마트</p>
-          </a>
-          <a href='' className='Navigation__Item'>
-            <Image
-              className='Navigation__img'
-              publicId='entertainment_b29fhy'
-              format='jpg'
-            ></Image>
-            <p>엔터테인먼트</p>
-          </a>
-        </div>
-      </CloudinaryContext>
+    <section className='navigation'>
+      <div className='navigation__wrap'>
+        {navigation.map((category) => {
+          return (
+            <div key={category.idCategory} className='navigation__btn'>
+              <img src={category.strIconSource} alt={category.strTitle} />
+              <span>{category.strTitle}</span>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- gem/src/datafiles/categories.js
+++ gem/src/datafiles/categories.js
@@ -1,18 +1,20 @@
-export const copywrites = [
+export const category = [
   {
     idCategory: '1',
     strCategory: 'hot',
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123317/hot_whb0c9.png',
     strTitle: '인기장소',
+    display: true,
     activated: true,
   },
   {
     idCategory: '2',
-    strCategory:
+    strCategory: 'new',
+    strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123317/new_v8vqmp.png',
-    strIconSource: '',
     strTitle: '새로생긴 곳',
+    display: true,
     activated: true,
   },
   {
@@ -21,6 +23,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/food_nuddsz.png',
     strTitle: '맛집',
+    display: true,
     activated: true,
   },
   {
@@ -29,6 +32,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/cafe_dogisg.png',
     strTitle: '카페',
+    display: true,
     activated: true,
   },
   {
@@ -37,6 +41,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123317/pub_ypq1dw.png',
     strTitle: '술집',
+    display: false,
     activated: true,
   },
   {
@@ -45,6 +50,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/fitness_osoaom.png',
     strTitle: '운동・피트니스',
+    display: true,
     activated: true,
   },
   {
@@ -53,6 +59,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/golf_rp6iid.png',
     strTitle: '스크린골프',
+    display: true,
     activated: true,
   },
   {
@@ -61,6 +68,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123317/salon_ciebqq.png',
     strTitle: '미용실',
+    display: true,
     activated: true,
   },
   {
@@ -69,6 +77,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/academy_vap3fs.png',
     strTitle: '학원',
+    display: true,
     activated: true,
   },
   {
@@ -77,6 +86,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123318/studio_fne8nw.png',
     strTitle: '스튜디오',
+    display: true,
     activated: true,
   },
   {
@@ -85,6 +95,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123317/pharmacy_ngaewq.png',
     strTitle: '약국',
+    display: true,
     activated: true,
   },
   {
@@ -93,6 +104,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123317/hospital_ttw46g.png',
     strTitle: '병원',
+    display: true,
     activated: true,
   },
   {
@@ -101,6 +113,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/dog_v7ba4i.png',
     strTitle: '애견',
+    display: true,
     activated: true,
   },
   {
@@ -109,6 +122,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123318/shopping_aevysq.png',
     strTitle: '마트',
+    display: true,
     activated: true,
   },
   {
@@ -117,6 +131,7 @@ export const copywrites = [
     strIconSource:
       'https://res.cloudinary.com/diimwnnmj/image/upload/v1616123316/entertainment_b29fhy.png',
     strTitle: '엔터테인먼트',
+    display: true,
     activated: true,
   },
 ];
