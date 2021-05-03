e6d0d2103e7b18d16241e8ba18805f3206af621c
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-05-03 10:50:27 +0900

    업체데이터 Airtable과 연결

--- functions/store-api.js
+++ functions/store-api.js
@@ -1,11 +1,74 @@
-const stores = require('../src/datafiles/storesApi');
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: 'key5AMdi7ejadTzUy' })
+  .base('appDzyBPyX5MjMkrU')
+  .table('stores');
 
 exports.handler = async (event, context) => {
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-    body: JSON.stringify(stores),
-  };
+  try {
+    const { records } = await airtable.list();
+    const stores = records.map((store) => {
+      const { id } = store;
+      const {
+        name,
+        shortDescription,
+        ownerMessage,
+        categoryTitle,
+        tags,
+        fullAddress,
+        sigunGu,
+        eupmyeondongRi,
+        streetName,
+        streetNumber,
+        buildingNumber,
+        mainMenu,
+        openHour,
+        closeHour,
+        breaktimeStart,
+        breaktimeEnd,
+        breakDays,
+        instagramAccount,
+        naverLink,
+        image,
+        ownerImage,
+        storeVerified,
+        phoneNumber,
+      } = store.fields;
+      return {
+        id,
+        name,
+        shortDescription,
+        ownerMessage,
+        categoryTitle,
+        tags,
+        fullAddress,
+        sigunGu,
+        eupmyeondongRi,
+        streetName,
+        streetNumber,
+        buildingNumber,
+        mainMenu,
+        openHour,
+        closeHour,
+        breaktimeStart,
+        breaktimeEnd,
+        breakDays,
+        instagramAccount,
+        naverLink,
+        image,
+        ownerImage,
+        storeVerified,
+        phoneNumber,
+      };
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(stores),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Server Error',
+    };
+  }
 };
