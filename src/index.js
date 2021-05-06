ec5711e4205b94dcb1a24b7dd8953275bc924c42
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-05-06 09:19:52 +0900

    index.js에 axios default 값을 넣고 테스트

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './components/App';
 import { AppProvider } from './components/context';
 import { StoreProvider } from './components/storeContext';
+import axios from 'axios';
+
+//create a new Airtable object in React
+new Airtable({ apiKey: 'APkey5AMdi7ejadTzUyI_KEY' }).base('appDzyBPyX5MjMkrU');
+//base endpoint to call with each request
+axios.defaults.baseURL = 'https://api.airtable.com/v0/appDzyBPyX5MjMkrU/stores';
+//content type to send with all POST requests
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+//authenticate to the base with the API key
+axios.defaults.headers['Authorization'] = 'Bearer API_KEY';
 
 ReactDOM.render(
   <React.StrictMode>
