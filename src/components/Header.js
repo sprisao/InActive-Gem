8da52f12df9f801f06a6299c5942b6b14d0419b9
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-05-08 16:09:57 +0900

    로고 및 파비콘 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ function Header() {
         <div className='header-logo'>
           <Link to='/'>
             <img
-              src='https://res.cloudinary.com/diimwnnmj/image/upload/v1620365110/%E1%84%80%E1%85%A9%E1%84%80%E1%85%A9%E1%84%8C%E1%85%A6%E1%86%B7BI_udvemv.png'
+              src='https://res.cloudinary.com/diimwnnmj/image/upload/v1620457721/%E1%84%80%E1%85%A9%E1%84%80%E1%85%A9%E1%84%8C%E1%85%A6%E1%86%B7BI_fpwc2t.png'
               className='logo'
               alt='logo'
             />
