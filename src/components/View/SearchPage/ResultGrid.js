9c52e33a284033a69610d2dc6f004c76389d7cb7
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-05-30 09:08:11 +0900

    검색창 가운데 이동 등

--- src/components/View/SearchPage/ResultGrid.js
+++ src/components/View/SearchPage/ResultGrid.js
@@ -3,11 +3,8 @@ import GridCard from '../../Grid/GridCard';
 import '../../Grid/Grid.css';
 
 const Grid = ({ filter }) => {
-  console.log(filter);
-
   return (
     <section className='title'>
-      <h3> 검색결과: </h3>
       <section className='grid'>
         {filter.map((store) => {
           return (
