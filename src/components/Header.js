a642a90980f6a2bd017655189547b2c31c41747f
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-05-31 13:03:11 +0900

    서치 아이콘 변경-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header() {
           <div className='searchIcon__container'>
             <img
               src='https://res.cloudinary.com/diimwnnmj/image/upload/v1622432596/%E1%84%8C%E1%85%A6%E1%84%86%E1%85%A9%E1%86%A8_%E1%84%8B%E1%85%A5%E1%86%B9%E1%84%82%E1%85%B3%E1%86%AB_%E1%84%8B%E1%85%A1%E1%84%90%E1%85%B3%E1%84%8B%E1%85%AF%E1%84%8F%E1%85%B3_rmmcly.png'
-              alt=''
+              alt='searchIcon'
             />
           </div>
         </Link>
