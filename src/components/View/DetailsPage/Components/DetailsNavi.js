0c991f57db4d437479e6081c9974476ce42465bf
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-06-01 11:28:48 +0900

    디테일 뒤로가기 버튼 수정

--- src/components/View/DetailsPage/Components/DetailsNavi.js
+++ src/components/View/DetailsPage/Components/DetailsNavi.js
@@ -15,7 +15,12 @@ const DetailsNavi = (props) => {
           return (
             <Link key={category.id} to={`/category/${props.categoryTitle}`}>
               <div className='detailsNavi__box'>
-                <FiChevronLeft className='detailsNavi__icon' />
+                <div className='detailsNavi__icon'>
+                  <img
+                    src='https://res.cloudinary.com/diimwnnmj/image/upload/v1622513949/%E1%84%8C%E1%85%A6%E1%84%86%E1%85%A9%E1%86%A8_%E1%84%8B%E1%85%A5%E1%86%B9%E1%84%82%E1%85%B3%E1%86%AB_%E1%84%8B%E1%85%A1%E1%84%90%E1%85%B3%E1%84%8B%E1%85%AF%E1%84%8F%E1%85%B3_3_zqkqpv.png'
+                    alt='뒤로가기'
+                  />
+                </div>
                 <div className='detailsNavi__contents'>
                   <p>{props.categoryTitle}</p>
                 </div>
