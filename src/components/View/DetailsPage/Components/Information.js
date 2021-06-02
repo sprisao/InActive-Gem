cae452ff30adad15b0ec4f640395b273d94b8504
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-06-02 11:21:18 +0900

    주소 및 전화번호 복사기능 추가

--- src/components/View/DetailsPage/Components/Information.js
+++ src/components/View/DetailsPage/Components/Information.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FiInstagram,
   FiClock,
@@ -7,7 +7,11 @@ import {
   FiChevronDown,
   FiPhoneOutgoing,
   FiMinusCircle,
+  FiCopy,
 } from 'react-icons/fi';
+
+import ReactDOM from 'react-dom';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import './Information.css';
 
@@ -15,20 +19,30 @@ import './Information.css';
 // 특정 필드에 데이터가 있을 경우에만 그 필드가 속한 'div'가 표시되면 좋겠다.
 
 const Information = (props) => {
+  const [copied, setCopied] = useState(false);
   let businessHours;
   if (props.breaktimeStart) {
     businessHours = (
-      <div className='businessHours'>
-        <FiClock style={{ strokeWidth: '1px' }} className='details__icon' />
-        <p>
-          {props.openHour} ~ {props.closeHour} 휴게시간 {props.breaktimeStart} ~
-          {props.breaktimeEnd}
-        </p>
-      </div>
+      <>
+        <div className='businessHours'>
+          <FiClock
+            style={{ strokeWidth: '1px', color: '#a8a8a8' }}
+            className='details__icon'
+          />
+          <p>
+            {props.openHour} ~ {props.closeHour} 휴게시간 {props.breaktimeStart}{' '}
+            ~{props.breaktimeEnd}
+          </p>
+        </div>
+        <hr />
+      </>
     );
   } else {
     <div className='businessHours'>
-      <FiClock style={{ strokeWidth: '1px' }} className='details__icon' />
+      <FiClock
+        style={{ strokeWidth: '1px', color: '#a8a8a8' }}
+        className='details__icon'
+      />
       <p>
         {props.openHour} ~ {props.closeHour}
       </p>
@@ -37,23 +51,54 @@ const Information = (props) => {
 
   // 휴무 필터링
   let breakDays;
+  console.log(props.breakDays.length);
 
   if (props.breakDays) {
     if (props.breakDays[0] === '연중무휴') {
       breakDays = (
-        <div className='breakDays'>
-          <FiXCircle style={{ strokeWidth: '1px' }} className='details__icon' />
-          <p>{props.breakDays}</p>
-        </div>
+        <>
+          <div className='breakDays'>
+            <FiXCircle
+              style={{ strokeWidth: '1px', color: '#a8a8a8' }}
+              className='details__icon'
+            />
+            <p>{props.breakDays}</p>
+          </div>
+          <hr />
+        </>
       );
-    } else {
+    }
+    if (props.breakDays[0] !== '연중무휴' && props.breakDays.length !== 1) {
       breakDays = (
-        <div className='breakDays'>
-          <FiXCircle style={{ strokeWidth: '1px' }} className='details__icon' />
-          <p>
-            {props.breakDays} <span style={{ color: 'black' }}>휴무</span>
-          </p>
-        </div>
+        <>
+          <div className='breakDays'>
+            <FiXCircle
+              style={{ strokeWidth: '1px', color: '#a8a8a8' }}
+              className='details__icon'
+            />
+            <p>
+              {props.breakDays} <span style={{ color: 'black' }}>휴무</span>
+            </p>
+          </div>
+          <hr />
+        </>
+      );
+    }
+    if (props.breakDays.length >= 2) {
+      breakDays = (
+        <>
+          <div className='breakDays'>
+            <FiXCircle
+              style={{ strokeWidth: '1px', color: '#a8a8a8' }}
+              className='details__icon'
+            />
+            {props.breakDays.map((breakDay) => (
+              <p>{breakDay}&nbsp;</p>
+            ))}
+            <span style={{ color: 'black' }}>휴무</span>
+          </div>
+          <hr />
+        </>
       );
     }
   }
@@ -63,15 +108,38 @@ const Information = (props) => {
       {businessHours}
       {breakDays}
       <div className='location'>
-        <FiMapPin style={{ strokeWidth: '1px' }} className='details__icon' />
+        <FiMapPin
+          style={{ strokeWidth: '1px', color: '#a8a8a8' }}
+          className='details__icon'
+        />
         <p>{props.fullAddress}</p>
+        <CopyToClipboard
+          text={props.fullAddress}
+          onCopy={() => setCopied(true)}
+        >
+          <div className='copyBtn__container'>
+            <FiCopy style={{ fontSize: '1rem' }} />
+            <p>복사</p>
+          </div>
+        </CopyToClipboard>
+        {/* {copied ? <span style={{ color: 'red' }}>Copied.</span> : null} */}
       </div>
+      <hr />
       <div className='phoneNumber'>
         <FiPhoneOutgoing
-          style={{ strokeWidth: '1px' }}
+          style={{ strokeWidth: '1px', color: '#a8a8a8' }}
           className='details__icon'
         />
         <p>{props.phoneNumber}</p>
+        <CopyToClipboard
+          text={props.phoneNumber}
+          onCopy={() => setCopied(true)}
+        >
+          <div className='copyBtn__container'>
+            <FiCopy style={{ fontSize: '1rem' }} />
+            <p>복사</p>
+          </div>
+        </CopyToClipboard>
       </div>
     </div>
   );
