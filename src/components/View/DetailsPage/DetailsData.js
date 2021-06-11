eaad023f9a5f1328623016c5042e2b6b2a89cc2e
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-06-11 23:12:54 +0900

    상세페이지 Data Fetch 방식 변경

--- src/components/View/DetailsPage/DetailsData.js
+++ src/components/View/DetailsPage/DetailsData.js
@@ -1,20 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import Loading from '../../Loading';
-import { useGlobalContext } from '../../context';
 import { useParams } from 'react-router-dom';
+import Airtable from 'airtable';
 
 import DetailsPage from './DetailsPage';
 
+const base = new Airtable({ apiKey: 'key5AMdi7ejadTzUy' }).base(
+  'appDzyBPyX5MjMkrU'
+);
+
 const DetailsData = () => {
   const { id } = useParams();
-  const { stores, loading } = useGlobalContext();
+  const [store, setStore] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const item = [];
+
+  const fetchStore = () => {
+    setLoading(true);
+    base('stores')
+      .select({
+        filterByFormula: `record_id = "${id}"`,
+        view: 'Grid view',
+        maxRecords: 1,
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          records.forEach(function (record) {
+            item.push({
+              id: record.id,
+              ...record._rawJson.fields,
+            });
+          });
+          fetchNextPage();
+          setStore(item);
+          setLoading(false);
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+  };
+
+  useEffect(() => {
+    fetchStore();
+  }, []);
 
   if (loading) {
     return <Loading />;
   }
   return (
     <>
-      {stores.map((store) => {
+      {store.map((store) => {
         if (store.id === id) {
           return <DetailsPage key={store.id} store={store}></DetailsPage>;
         } else return null;
