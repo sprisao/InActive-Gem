0b7129db3a562cdfbfd6508dad6fd83466c71542
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-06-21 01:11:58 +0900

    Modal 팝업 속도 조정

--- src/components/View/DetailsPage/Components/Modal.js
+++ src/components/View/DetailsPage/Components/Modal.js
@@ -1,14 +1,14 @@
-import React from "react";
-import "./Modal.css";
+import React from 'react';
+import './Modal.css';
 
 const Modal = (props) => {
   //   const { name } = props;
 
   return (
     // 모달이 열릴때 openModal 클래스가 생성된다.
-    <div className="openModal modal">
-      <section className="modalBackground">
-        <main className="modalMain">복사 되었습니다 🔖 </main>
+    <div className='openModal modal'>
+      <section className='modalBackground'>
+        <main className='modalMain'>복사 되었습니다 ✅ </main>
       </section>
     </div>
   );
