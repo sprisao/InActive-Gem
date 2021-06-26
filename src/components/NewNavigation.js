294de68d189b90ec1d86757a359ff36be7e1ff45
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-06-26 10:50:48 +0900

    [v. 1.5 Update] Navigation 리뉴얼-2

--- src/components/NewNavigation.js
+++ src/components/NewNavigation.js
@@ -53,28 +53,92 @@ const NewNavigation = () => {
         </div>
         <div className='ThirdGrid__Wrap'>
           <div className='ThirdGrid__Item'>
-            <h2>호프・주점</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>호프・주점</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/beer-mug_1f37a.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>운동・헬스</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>운동・헬스</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/flexed-biceps_medium-light-skin-tone_1f4aa-1f3fc_1f3fc.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>미용・뷰티</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>미용・뷰티</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/lipstick_1f484.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>스튜디오</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>스튜디오</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/camera_1f4f7.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>게임・노래</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>게임・노래</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/video-game_1f3ae.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>꽃</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>꽃</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/tulip_1f337.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>키즈</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>키즈</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/teddy-bear_1f9f8.png'
+                alt=''
+              />
+            </div>
           </div>
           <div className='ThirdGrid__Item'>
-            <h2>캠핑장</h2>
+            <div className='Navigation__Item__Header'>
+              <h2>캠핑장</h2>
+            </div>
+            <div className='Navigation__Item__Emoji__Container'>
+              <img
+                src='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/camping_1f3d5-fe0f.png'
+                alt=''
+              />
+            </div>
           </div>
         </div>
         <div className='FourthGrid__Wrap'>
