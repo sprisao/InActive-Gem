5a98ff518ad04015898a565b95201377db935e67
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-07-04 18:36:00 +0900

    디테일페이지 데이터 불러오는 방식 변경 및 카테고리, 디테일 페이지 margintop 제거

--- src/components/View/DetailsPage/DetailsData.js
+++ src/components/View/DetailsPage/DetailsData.js
@@ -1,60 +1,60 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Loading from '../../Loading';
+import { useGlobalContext } from '../../context';
 import { useParams } from 'react-router-dom';
-import Airtable from 'airtable';
 
 import DetailsPage from './DetailsPage';
 
-const base = new Airtable({ apiKey: 'key5AMdi7ejadTzUy' }).base(
-  'appDzyBPyX5MjMkrU'
-);
-
 const DetailsData = () => {
+  const { cafes, restaurants, loading, stores } = useGlobalContext();
   const { id } = useParams();
-  const [store, setStore] = useState([]);
-  const [loading, setLoading] = useState(true);
 
-  const item = [];
+  const allStores = stores.concat(restaurants, cafes);
 
-  const fetchStore = () => {
-    setLoading(true);
-    base('stores')
-      .select({
-        filterByFormula: `record_id = "${id}"`,
-        view: 'Grid view',
-        maxRecords: 1,
-      })
-      .eachPage(
-        function page(records, fetchNextPage) {
-          records.forEach(function (record) {
-            item.push({
-              id: record.id,
-              ...record._rawJson.fields,
-            });
-          });
-          fetchNextPage();
-          setStore(item);
-          setLoading(false);
-        },
-        function done(err) {
-          if (err) {
-            console.error(err);
-            return;
-          }
-        }
-      );
-  };
+  // const [store, setStore] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetchStore();
-  }, [id]);
+  // const item = [];
+
+  // const fetchStore = () => {
+  //   setLoading(true);
+  //   base('stores')
+  //     .select({
+  //       filterByFormula: `record_id = "${id}"`,
+  //       view: 'Grid view',
+  //       maxRecords: 1,
+  //     })
+  //     .eachPage(
+  //       function page(records, fetchNextPage) {
+  //         records.forEach(function (record) {
+  //           item.push({
+  //             id: record.id,
+  //             ...record._rawJson.fields,
+  //           });
+  //         });
+  //         fetchNextPage();
+  //         setStore(item);
+  //         setLoading(false);
+  //       },
+  //       function done(err) {
+  //         if (err) {
+  //           console.error(err);
+  //           return;
+  //         }
+  //       }
+  //     );
+  // };
+
+  // useEffect(() => {
+  //   fetchStore();
+  // }, [id]);
 
   if (loading) {
     return <Loading />;
   }
   return (
     <>
-      {store.map((store) => {
+      {allStores.map((store) => {
         if (store.id === id) {
           return <DetailsPage key={store.id} store={store}></DetailsPage>;
         } else return null;
