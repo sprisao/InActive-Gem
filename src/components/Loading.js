c975690892ba437c282f9861e44abe857de7a6d3
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-08-15 13:19:49 +0900

    로딩 gif 배경 제거

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,8 +6,8 @@ const Loading = () => {
     <section className='loading'>
       <div className='loading__contents'>
         <img
-          src='https://res.cloudinary.com/diimwnnmj/image/upload/v1625989660/KakaoTalk_Photo_2021-07-11-16-47-27_yfwpec.gif'
-          alt=''
+          src='https://res.cloudinary.com/diimwnnmj/image/upload/v1629001126/KakaoTalk_Photo_2021-08-15-13-17-13_eluzaj.gif'
+          alt='Loading...'
         />
       </div>
     </section>
