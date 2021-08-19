169e14ef13a8b8b845003f70978c48214a22036e
Bruce Choe <brucechoe@Bruceui-MacBookPro.local>
2021-08-19 22:54:54 +0900

    DetailsPage > Swiper - 디테일 변경

--- src/components/View/DetailsPage/Components/DetailsNavi.js
+++ src/components/View/DetailsPage/Components/DetailsNavi.js
@@ -15,19 +15,19 @@ const DetailsNavi = (props) => {
     <section className='detailsNavi'>
       <div className='detailsNavi__box'>
         <div className='Details__Backward' onClick={goBack}>
-          <FiChevronLeft style={{ fontSize: '1.85rem', color: '#454545' }} />
+          <FiChevronLeft style={{ fontSize: '1.85rem', color: '#fff' }} />
         </div>
-        <div className='detailsNavi__contents'>
+        {/* <div className='detailsNavi__contents'>
           <h3>{props.name}</h3>
-        </div>
-        <div className='Details__Save'>
+        </div> */}
+        {/* <div className='Details__Save'>
           <FiBookmark
             style={{
               fontSize: '1.85rem',
               strokeWidth: '1.25px',
             }}
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
