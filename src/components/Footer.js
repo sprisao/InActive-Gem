2aff62698540499fd26039cac855449a5397de92
Bruce Choe <whdtmek@gmail.com>
2021-09-03 14:17:19 +0900

    docs & style: 하단안내사항 및 여백 추가

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -101,6 +101,12 @@ const Footer = () => {
         </div>
         <div className='Copyrights'>
           <p>Copyright © 2020 Bruce Corp. All rights reserved.</p>
+          <br />
+          <p>
+            브루스코퍼레이션은 통신판매중개자이며, 통신판매의 당사자가 아닙니다.
+            또한 판매자와 구매자간의 직거래에 대하여 당사는 관여하지 않기 때문에
+            거래에 대해서는 책임을 지지 않습니다.
+          </p>
         </div>
         <div className='Policies'>
           <a href='/useragreements'>이용약관</a>
