1a74bf4bd37bcf53b1672b2ec71f8c985118f3a8
Bruce Choe <whdtmek@gmail.com>
2021-09-11 10:32:15 +0900

    chore: 상세페이지 이벤트리스트가 데스크탑모드에서 빠져나오는것 수정

--- src/components/View/DetailsPage/Components/DetailsEventsList.js
+++ src/components/View/DetailsPage/Components/DetailsEventsList.js
@@ -2,26 +2,30 @@ import React from 'react';
 
 import { useGlobalContext } from '../../../context';
 
-import '../../EventsPage/EventsPage.css';
+import './DetailsEventsList.css';
 
 const DetailsEventsList = (props) => {
   const { loading, events } = useGlobalContext();
 
   return (
     <div className='EventsPage__Wrapper'>
-      <h3 style={{ marginTop: '1rem', fontSize: '1.4rem' }}>진행중인 이벤트</h3>
-      <div className='EventsWrapper'>
-        {events.map((item) => {
-          if (item.id === props.eventItems[0])
-            return (
-              // <Link to={`/store/${item.stores[0]}`} className=''>
-              <div className='EventsCard' key={item.id}>
-                <img src={item.image[0].url} alt='' />
-              </div>
-              // </Link>
-            );
-        })}
-      </div>
+      <section className='Details-EventsList'>
+        <h3 style={{ marginTop: '1rem', fontSize: '1.4rem' }}>
+          진행중인 이벤트
+        </h3>
+        <div className='EventsWrapper'>
+          {events.map((item) => {
+            if (item.id === props.eventItems[0])
+              return (
+                // <Link to={`/store/${item.stores[0]}`} className=''>
+                <div className='EventsCard' key={item.id}>
+                  <img src={item.image[0].url} alt='' />
+                </div>
+                // </Link>
+              );
+          })}
+        </div>
+      </section>
     </div>
   );
 };
