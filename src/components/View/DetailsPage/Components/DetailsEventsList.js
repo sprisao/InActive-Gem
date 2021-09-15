5ddb2b98e0b1d648150b19ca6d2111a85022bebe
Bruce Choe <whdtmek@gmail.com>
2021-09-15 21:46:59 +0900

    feat: 이벤트 상세페이지 추가 & 리액트 마크다운리더 추가

--- src/components/View/DetailsPage/Components/DetailsEventsList.js
+++ src/components/View/DetailsPage/Components/DetailsEventsList.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import ReactMarkdown from 'react-markdown';
 import { useGlobalContext } from '../../../context';
 
 import './DetailsEventsList.css';
 
 const DetailsEventsList = (props) => {
   const { loading, events } = useGlobalContext();
-
+  console.log(events);
+  console.log(props.eventItems[0]);
   return (
     <div className='EventsPage__Wrapper'>
       <section className='Details-EventsList'>
@@ -15,13 +16,60 @@ const DetailsEventsList = (props) => {
         </h3>
         <div className='EventsWrapper'>
           {events.map((item) => {
+            console.log(item.id);
             if (item.id === props.eventItems[0])
               return (
-                // <Link to={`/store/${item.stores[0]}`} className=''>
-                <div className='EventsCard' key={item.id}>
-                  <img src={item.image[0].url} alt='' />
-                </div>
-                // </Link>
+                <>
+                  <div className='EventsCard' key={item.id}>
+                    <img src={item.image[0].url} alt='' />
+                  </div>
+                  <div className='EventDetails'>
+                    <div className='EventDetails-Wrapper'>
+                      <div className='EventDetail'>
+                        <h4>이벤트 종류</h4>
+                        <div className='EventDetail-ItemWrapper'>
+                          <div className='EventDetail-Item'>
+                            <p>{item.type}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='EventDetail'>
+                        <h4>이벤트 기간</h4>
+                        <div className='EventDetail-ItemWrapper'>
+                          <div className='EventDetail-Item'>
+                            <p>{item.period}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='EventDetail'>
+                        <h4>대상</h4>
+                        <div className='EventDetail-ItemWrapper'>
+                          <div className='EventDetail-Item'>
+                            <p>{item.target}</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='EventDetail'>
+                        <h4>혜택</h4>
+                        <div className='EventDetail-ItemWrapper'>
+                          {item.benefits.map((benefit) => (
+                            <div className='EventDetail-Item'>
+                              <p>{benefit}</p>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <div className='EventDetail'>
+                        <h4>상세내용</h4>
+                        <div className='EventDetail-ItemWrapper'>
+                          <div className='EventDetail-Item--Details'>
+                            <ReactMarkdown>{item.details}</ReactMarkdown>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
               );
           })}
         </div>
