838ed74af946a9695d0afae9ceeb54eb7a1939d6
Bruce Choe <whdtmek@gmail.com>
2021-10-19 16:45:09 +0900

    feat: RegisterPage> Checkbox 추가

--- src/components/View/LoginPage/RegisterPage.js
+++ src/components/View/LoginPage/RegisterPage.js
@@ -18,6 +18,7 @@ const RegisterPage = () => {
     watch,
     formState: { errors },
     handleSubmit,
+    getValues,
   } = useForm({ mode: 'onChange' });
   const [loading, setLoading] = useState(false);
 
@@ -57,22 +58,20 @@ const RegisterPage = () => {
       email: user.email,
       profile_picture: user.photoURL,
     });
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(data, null, 4));
   };
+
+  const atLeastOne = () =>
+    getValues('test').length ? true : 'Please tell me if this is too hard.';
+
   return (
     <div className='auth-wrapper'>
       <div style={{ textAlign: 'center' }}>
-        <h3>가입하기</h3>
+        <h3>회원가입</h3>
       </div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Email</label>
-        <input
-          name='email'
-          type='email'
-          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-        />
-        {errors.email && <p>이메일은 필수입력 사항입니다.</p>}
-
-        <label>Name</label>
+        <label>이름</label>
         <input
           name='name'
           type='name'
@@ -84,22 +83,26 @@ const RegisterPage = () => {
         {errors.name && errors.name.type === 'maxLength' && (
           <p>이름은 7자 이상 사용하실 수 없습니다.</p>
         )}
-        {errors.name === 'maxLength' && <p>Your input exceed maximum length</p>}
-
-        <label>Password</label>
+        <label>이메일</label>
+        <input
+          name='email'
+          type='email'
+          {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>이메일 형식에 맞게 입력해주세요!</p>}
+        <label>비밀번호(8자 이상, 문자/숫자/기호 사용 가능)</label>
         <input
           name='password'
           type='password'
-          {...register('password', { required: true, minLength: 6 })}
+          {...register('password', { required: true, minLength: 8 })}
         />
         {errors.password && errors.password.type === 'required' && (
           <p>비밀번호는 필수입력 사항입니다.</p>
         )}
         {errors.password && errors.password.type === 'minLength' && (
-          <p>Password must have at least 6 Len</p>
+          <p>비밀번호는 최소 8자 이상이어야 합니다.</p>
         )}
-
-        <label>Password Confirm</label>
+        <label>비밀번호 확인</label>
         <input
           name='password_confirm'
           type='password'
@@ -116,8 +119,26 @@ const RegisterPage = () => {
           errors.password_confirm.type === 'validate' && (
             <p>입력하신 비밀번호와 일치하지 않습니다</p>
           )}
-        <input type='submit' disabled={loading} />
-        <Link to='login'>이미 아이디가 있다면</Link>
+        <div className='agreement-check'>
+          <input
+            name='agreement'
+            id='checkbox-input'
+            value={true}
+            type='checkbox'
+            label=' Example 3 (Controller)'
+            {...register('agreement', { required: true })}
+          ></input>
+          <label htmlFor='acceptTerms' className='form-check-label'>
+            Accept Terms & Conditions
+          </label>
+          <div className='invalid-feedback'>
+            {errors.agreement && errors.agreement.type === 'required' && (
+              <p>동의가 필요합니다.</p>
+            )}
+          </div>
+        </div>
+        <input type='submit' disabled={loading}></input>
+        <Link to='login'>이미 아이디가 있나요?</Link>
       </form>
     </div>
   );
