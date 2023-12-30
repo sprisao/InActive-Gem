0b0aaeb6af35f4327d19a2971103da52877e14bc
brucechoe <whdtmek@gmail.com>
2023-12-30 22:45:07 +0900

    Modify CSS and add logout functionality on landing and search pages
    Implemented updates to the CSS of the Search Page and added a more user-friendly styling. Also added logout functionality on Landing and Search Page increasing security and improving user flow. Changes also include switching to on-demand data loading for 'SecondCategory' and 'Store' data.

--- src/components/View/LandingPage/LandingPage.js
+++ src/components/View/LandingPage/LandingPage.js
@@ -1,26 +1,109 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 import SearchBox from '../SearchPage/SearchBox';
 import Home from '../../Home';
-import RCCMD from './Sections/RCCMD/RCCMD';
 import BottomNavigation from '../../BottomNavigation/BottomNavigation';
 import Footer from '../../Footer';
 
 import Separator from '../../Separator';
 
 import './LandingPage.css';
+import {getAuth, signOut} from "firebase/auth";
+import {useDetectOutsideClick} from "../CategoryPage/CategoryPageComponent/useDetectOutsideClick";
+import {FaUserCircle} from "react-icons/fa";
 
 const LandingPage = (history) => {
-  return (
-    <>
-      <SearchBox history={history} />
-      {/* <Header /> */}
-      <BottomNavigation activatedButton={'home'} />
-      <Home history={history} />
-      <Footer />
-    </>
-  );
-  // }
+    const auth = getAuth();
+    const dropdownRef = useRef(null);
+    const [dropActive, setDropActive] = useDetectOutsideClick(dropdownRef, false);
+    const user = auth.currentUser;
+
+    const handlePush = () => {
+        console.log(history);
+        history.history.push('/login');
+    };
+
+    const handleLogout = () => {
+        signOut(auth)
+            .then(() => {
+                // Sign-out successful.ㅇ
+                console.log('로그아웃 되었습니다');
+            })
+            .catch((error) => {
+                // An error happened.
+            });
+    };
+
+    const dropHandler = () => setDropActive(!dropActive);
+    return (
+        <>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                /*lightgray shadow*/
+                boxShadow: '0 0 0.5rem 0.1rem lightgray',
+                borderRadius: '0 0 0.5rem 0.5rem',
+                padding: '0.5rem 1rem 0.5rem 0.5rem',
+                backgroundColor: '#f5f5f5',
+            }}>
+                <div style={{
+                    width: '60px',
+                    height: '60px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}>
+                    <img
+                        style={{
+                            width: '70px',
+                            height: '70px',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }
+                        }
+                        src='/gem-logo.png'
+                        alt='로고'
+                    />
+                </div>
+                {/* <div className='Notification_Button'>
+          <FaBell />
+        </div> */}
+                <div className='Profile_Button' onClick={dropHandler}>
+                    <FaUserCircle/>
+                    <nav
+                        ref={dropdownRef}
+                        className={`Profile_Drop_Menu ${
+                            dropActive ? 'active' : 'inactive'
+                        }`}
+                    >
+                        <ul>
+                            {user ? (
+                                <>
+                                    <li>
+                                        <button onClick={handleLogout}>로그아웃</button>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li>
+                                        <button onClick={handlePush}>로그인</button>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <SearchBox history={history}/>
+            <BottomNavigation activatedButton={'home'}/>
+            <Home history={history}/>
+            <Footer/>
+        </>
+    );
+    // }
 };
 
 export default LandingPage;
--- src/components/View/SearchPage/SearchBox.js
+++ src/components/View/SearchPage/SearchBox.js
@@ -1,99 +1,50 @@
-import React, { useState, useRef } from 'react';
+import React, {useState, useRef} from 'react';
 
-import { useDetectOutsideClick } from '../CategoryPage/CategoryPageComponent/useDetectOutsideClick';
+import {useDetectOutsideClick} from '../CategoryPage/CategoryPageComponent/useDetectOutsideClick';
 
-import { FaUserCircle } from 'react-icons/fa';
-import { RiSearchLine } from 'react-icons/ri';
+import {FaUserCircle} from 'react-icons/fa';
+import {RiSearchLine} from 'react-icons/ri';
 
-import { getAuth, signOut } from 'firebase/auth';
+import {getAuth, signOut} from 'firebase/auth';
 import './SearchBox.css';
 
-const SearchBox = ({ history }) => {
-  const dropdownRef = useRef(null);
-  const [dropActive, setDropActive] = useDetectOutsideClick(dropdownRef, false);
+const SearchBox = ({history}) => {
 
-  const [userInput, setUserInput] = useState('');
-  const auth = getAuth();
-  const user = auth.currentUser;
+    const [userInput, setUserInput] = useState('');
 
-  const handlePush = () => {
-    console.log(history);
-    history.history.push('/login');
-  };
+    const sendUserInput = (e) => {
+        const {value} = e.target;
+        console.log('입력값', value);
+        setUserInput(value);
+    };
 
-  const handleLogout = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.ㅇ
-        console.log('로그아웃 되었습니다');
-      })
-      .catch((error) => {
-        // An error happened.
-      });
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (userInput) {
+            history.history.push(`/search/${userInput}`);
+        }
+    };
 
-  const dropHandler = () => setDropActive(!dropActive);
-  const sendUserInput = (e) => {
-    const { value } = e.target;
-    console.log('입력값', value);
-    setUserInput(value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (userInput) {
-      history.history.push(`/search/${userInput}`);
-    }
-  };
-
-  return (
-    <div className='top'>
-      <section className='SearchBox-Wrapper'>
-        <div className='SearchBox__Container'>
-          <form className='SearchBox' onSubmit={handleSubmit}>
-            <input
-              className='inputBox'
-              type='search'
-              name='search'
-              placeholder='가게이름 또는 메뉴로 검색'
-              onChange={sendUserInput}
-            />
-            <div className='SubmitButtonCase' onClick={handleSubmit}>
-              <RiSearchLine />
-            </div>
-          </form>
+    return (
+        <div className='top'>
+            <section className='SearchBox-Wrapper'>
+                <div className='SearchBox__Container'>
+                    <form className='SearchBox' onSubmit={handleSubmit}>
+                        <input
+                            className='inputBox'
+                            type='search'
+                            name='search'
+                            placeholder='가게이름 또는 메뉴로 검색'
+                            onChange={sendUserInput}
+                        />
+                        <div className='SubmitButtonCase' onClick={handleSubmit}>
+                            <RiSearchLine/>
+                        </div>
+                    </form>
+                </div>
+            </section>
         </div>
-        {/* <div className='Notification_Button'>
-          <FaBell />
-        </div> */}
-        <div className='Profile_Button' onClick={dropHandler}>
-          <FaUserCircle />
-          <nav
-            ref={dropdownRef}
-            className={`Profile_Drop_Menu ${
-              dropActive ? 'active' : 'inactive'
-            }`}
-          >
-            <ul>
-              {user ? (
-                <>
-                  <li>
-                    <button onClick={handleLogout}>로그아웃</button>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <button onClick={handlePush}>로그인</button>
-                  </li>
-                </>
-              )}
-            </ul>
-          </nav>
-        </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default SearchBox;
